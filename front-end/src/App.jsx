75b29e7b743a24be89a470c33c113010ca85b298
traizooo <michal.owsiejew23@gmail.com>
2024-04-16 20:54:11 +0200

    First Commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Home from "../components/home/Home.jsx";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
