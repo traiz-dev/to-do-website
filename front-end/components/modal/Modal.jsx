4e061cd1f563f5478bdc17b965c9812268c994e2
traizooo <michal.owsiejew23@gmail.com>
2024-04-22 23:37:46 +0200

    Commit

--- front-end/components/modal/Modal.jsx
+++ front-end/components/modal/Modal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { FiX } from "react-icons/fi";
 import { URL } from "/utils/consts";
+import { toast, Toaster } from 'react-hot-toast'
 
 const Modal = ({ isModalOpen, onClose }) => {
   const [title, setTitle] = useState("")
@@ -10,10 +11,21 @@ const Modal = ({ isModalOpen, onClose }) => {
   const [priority, setPriority] = useState(0)
 
   const onSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       const formattedStartDate = startDate.replace("T", " ") + ":00";
       const formattedEndDate = endDate.replace("T", " ") + ":00";
+
+      if (
+        title.length === 0 ||
+        startDate.length === 0 ||
+        endDate.length === 0 ||
+        description.length === 0 ||
+        priority === 0
+      ) {
+        toast.error("You need to fill in all input fields!");
+        return;
+      }
 
       const response = await fetch(URL + "/api/create_task", {
         method: "POST",
@@ -30,10 +42,16 @@ const Modal = ({ isModalOpen, onClose }) => {
       });
 
       if (!response.ok) {
-        console.log("There was an error with a response!");
+        toast.error("There was an error with a server response!");
       }
 
-      console.log("Data submitted successfully!");
+      setTimeout(() => {
+        toast.success("Task created successfully!");
+        setTimeout(() => {
+          window.location.reload();
+        }, 1500);
+      }, 500);
+
     } catch(error) {
       console.error("There was an error! " + error.message)
     }
@@ -41,13 +59,14 @@ const Modal = ({ isModalOpen, onClose }) => {
 
   return (
     <>
+      <Toaster position="top-center" reverseOrder={false} />
       {isModalOpen && (
         <>
           <div className="fixed left-0 top-0 z-50 w-full h-full bg-black bg-opacity-70 flex items-center justify-center">
-            <div className="absolute left-[32%] top-[2%] z-20">
+            <div className="absolute left-[31%] top-[2%] z-20">
               <button
                 onClick={onClose}
-                className="absolute top-[125px] right-6 text-white font-bold hover:text-gray-600 transition-all duration-150"
+                className="absolute top-[107px] right-6 text-white font-bold hover:text-gray-600 transition-all duration-150"
               >
                 <FiX />
               </button>
@@ -63,11 +82,11 @@ const Modal = ({ isModalOpen, onClose }) => {
                     <input
                       type="text"
                       className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 w-full md:w-[100%] mr-[2%]"
-                      placeholder="Title"
+                      placeholder="Name"
                       onChange={(e) => setTitle(e.target.value)}
                     />
                     <h2 className="text-l uppercase font-bold text-gray-200 mb-4 w-full">
-                      Start date and time.
+                      Start date and time
                     </h2>
                     <input
                       type="datetime-local"
@@ -76,7 +95,7 @@ const Modal = ({ isModalOpen, onClose }) => {
                       onChange={(e) => setStartDate(e.target.value)}
                     />
                     <h2 className="text-l uppercase font-bold text-gray-200 mb-4 w-full">
-                      End date and time.
+                      End date and time
                     </h2>
                     <input
                       type="datetime-local"
@@ -84,6 +103,9 @@ const Modal = ({ isModalOpen, onClose }) => {
                       placeholder="End Date and Time"
                       onChange={(e) => setEndDate(e.target.value)}
                     />
+                    <h2 className="text-l uppercase font-bold text-gray-200 mb-4 w-full">
+                      Priority
+                    </h2>
                     <select
                       id="prio"
                       className="w-full mr-2 mb-4"
@@ -105,6 +127,7 @@ const Modal = ({ isModalOpen, onClose }) => {
                     ></textarea>
 
                     <button
+                      type="submit"
                       onClick={onSubmit}
                       className="uppercase w-full bg-gradient-to-r  from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition duration-150"
                     >
--- front-end/components/task-card/TaskCard.jsx
+++ front-end/components/task-card/TaskCard.jsx
@@ -5,6 +5,7 @@ import { MdOutlineDoneAll } from "react-icons/md";
 import { BiTask } from "react-icons/bi";
 import { BiTaskX } from "react-icons/bi";
 import { URL } from "/utils/consts";
+import toast, { Toaster } from "react-hot-toast";
 
 const TaskCard = ({ tasks, setTasks }) => {
   const [gridCols, setGridCols] = useState("grid-cols-1");
@@ -22,7 +23,7 @@ const TaskCard = ({ tasks, setTasks }) => {
       }
     };
 
-    handleResize(); // Initial check
+    handleResize();
     window.addEventListener("resize", handleResize);
 
     return () => {
@@ -69,137 +70,147 @@ const TaskCard = ({ tasks, setTasks }) => {
       });
 
       if (!response.ok) {
-        throw new Error("Failed to delete post!");
+        toast.error("There was an error with a server response!");
       }
 
       const updatedTasks = tasks.filter((task) => task.id !== task_id);
       setTasks(updatedTasks);
+      toast.success("Task deleted successfully!")
     } catch (error) {
       console.error("There was an error: " + error.message);
     }
   };
 
   return (
-    <div className="flex justify-center">
-      <div className={`grid gap-6 ${gridCols} mt-10 pb-10`}>
-        {tasks.map((task) => {
-          const lastUpdatedISO = new Date(task.lastUpdated).toISOString();
-          const lastUpdatedDate = new Date(lastUpdatedISO);
-          const day = lastUpdatedDate.getDate() -1 ;
-          const month = lastUpdatedDate.toLocaleString("default", {
-            month: "short",
-          });
-          const year = lastUpdatedDate.getFullYear();
-          const weekday = lastUpdatedDate.toLocaleDateString("en-US", {
-            weekday: "short",
-          });
-          const formattedDate = `${weekday}, ${day} ${month
-            .toUpperCase()
-            .slice(0, 3)} ${year}`;
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="flex justify-center">
+        <div className={`grid gap-6 ${gridCols} mt-10 pb-10`}>
+          {tasks.map((task) => {
+            const lastUpdatedISO = new Date(task.lastUpdated).toISOString();
+            const lastUpdatedDate = new Date(lastUpdatedISO);
+            const day = lastUpdatedDate.getDate() - 1;
+            const month = lastUpdatedDate.toLocaleString("default", {
+              month: "short",
+            });
+            const year = lastUpdatedDate.getFullYear();
+            const weekday = lastUpdatedDate.toLocaleDateString("en-US", {
+              weekday: "short",
+            });
+            const formattedDate = `${weekday}, ${day} ${month
+              .toUpperCase()
+              .slice(0, 3)} ${year}`;
 
-            
+            let svgIcon;
 
-          let svgIcon;
+            if (task.priorityLevel === 3) {
+              svgIcon = <FaExclamation className="text-green-500" />;
+            } else if (task.priorityLevel === 2) {
+              svgIcon = (
+                <span className="flex">
+                  <FaExclamation className="text-orange-500" />
+                  <FaExclamation className="ml-[-8px] text-orange-500" />
+                </span>
+              );
+            } else if (task.priorityLevel === 1) {
+              svgIcon = (
+                <span className="flex">
+                  <FaExclamation className="text-red-600" />
+                  <FaExclamation className="ml-[-8px] text-red-600" />
+                  <FaExclamation className="ml-[-8px] text-red-600" />
+                </span>
+              );
+            }
 
-          if (task.priorityLevel === 3) {
-            svgIcon = <FaExclamation className="text-green-500" />;
-          } else if (task.priorityLevel === 2) {
-            svgIcon = (
-              <span className="flex">
-                <FaExclamation className="text-orange-500" />
-                <FaExclamation className="ml-[-8px] text-orange-500" />
-              </span>
-            );
-          } else if (task.priorityLevel === 1) {
-            svgIcon = (
-              <span className="flex">
-                <FaExclamation className="text-red-600" />
-                <FaExclamation className="ml-[-8px] text-red-600" />
-                <FaExclamation className="ml-[-8px] text-red-600" />
-              </span>
-            );
-          }
-
-          return (
-            <div
-              key={task.id}
-              className="relative bg-white w-80 h-60 rounded-md py-4 px-6 border hover:bg-gray-200 duration-500"
-            >
-              <p className="absolute top-5 left-5">{svgIcon}</p>
-              <div className="flex justify-center font-bold text-xl text-gray-800 pb-2 mr-3">
-                {/* {task.isDone ? <BiTask /> : <BiTaskX />} */}
-                {task.isDone ? <p className="text-green-500 uppercase">done</p> : <p className="text-red-500 uppercase"> not done</p>}
-              </div>
-              <h3 className="text-base font-semibold text-gray-900 text-nowrap overflow-hidden overflow-ellipsis">
-                {task.title}
-              </h3>
-              <p className="text-xs text-gray-500 pb-4 text-nowrap overflow-hidden overflow-ellipsis">
-                {task.content}
-              </p>
-              <div className="flex justify-center pb-5">
-                <button
-                  className={`relative flex justify-center w-full py-2 px-8 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r ${
-                    task.isDone
-                      ? "before:from-green-600"
-                      : "before:from-red-600"
-                  } ${
-                    task.isDone ? "before:to-green-400" : "before:to-red-400"
-                  } before:transition-all before:duration-800 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0`}
-                  onClick={() => updateIsDone(task.id)}
-                >
-                  {task.isDone ? <MdOutlineDoneAll /> : <MdOutlineRemoveDone />}
-                </button>
-              </div>
-              <div className="flex gap-2 text-sm text-gray-500 border-b pb-2">
-                <p className="">last update:</p>
-                <p>{formattedDate}</p>
-              </div>
-              <div className="flex justify-around items-center py-3">
-                <button className="flex gap-2 text-gray-600 hover:scale-110 duration-200 hover:cursor-pointer">
-                  <svg
-                    className="w-6 stroke-green-700"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+            return (
+              <div
+                key={task.id}
+                className="relative bg-white w-80 h-60 rounded-md py-4 px-6 border hover:bg-gray-200 duration-500"
+              >
+                <p className="absolute top-5 left-5">{svgIcon}</p>
+                <div className="flex justify-center font-bold text-xl text-gray-800 pb-2 mr-3">
+                  {/* {task.isDone ? <BiTask /> : <BiTaskX />} */}
+                  {task.isDone ? (
+                    <p className="text-green-500 uppercase">done</p>
+                  ) : (
+                    <p className="text-red-500 uppercase"> not done</p>
+                  )}
+                </div>
+                <h3 className="text-base font-semibold text-gray-900 text-nowrap overflow-hidden overflow-ellipsis">
+                  {task.title}
+                </h3>
+                <p className="text-xs text-gray-500 pb-4 text-nowrap overflow-hidden overflow-ellipsis">
+                  {task.content}
+                </p>
+                <div className="flex justify-center pb-5">
+                  <button
+                    className={`relative flex justify-center w-full py-2 px-8 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r ${
+                      task.isDone
+                        ? "before:from-green-600"
+                        : "before:from-red-600"
+                    } ${
+                      task.isDone ? "before:to-green-400" : "before:to-red-400"
+                    } before:transition-all before:duration-800 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0`}
+                    onClick={() => updateIsDone(task.id)}
                   >
-                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                  </svg>
-                  <div className="font-semibold text-sm text-green-700 mt-[3px]">
-                    Edit
-                  </div>
-                </button>
-                <button
-                  className="flex gap-2 text-gray-600 hover:scale-110 duration-200 hover:cursor-pointer"
-                  onClick={() => deleteTask(task.id)}
-                >
-                  <svg
-                    className="w-6 stroke-red-700"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+                    {task.isDone ? (
+                      <MdOutlineDoneAll />
+                    ) : (
+                      <MdOutlineRemoveDone />
+                    )}
+                  </button>
+                </div>
+                <div className="flex gap-2 text-sm text-gray-500 border-b pb-2">
+                  <p className="">last update:</p>
+                  <p>{formattedDate}</p>
+                </div>
+                <div className="flex justify-around items-center py-3">
+                  <button className="flex gap-2 text-gray-600 hover:scale-110 duration-200 hover:cursor-pointer">
+                    <svg
+                      className="w-6 stroke-green-700"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                    </svg>
+                    <div className="font-semibold text-sm text-green-700 mt-[3px]">
+                      Edit
+                    </div>
+                  </button>
+                  <button
+                    className="flex gap-2 text-gray-600 hover:scale-110 duration-200 hover:cursor-pointer"
+                    onClick={() => deleteTask(task.id)}
                   >
-                    <polyline points="3 6 5 6 21 6"></polyline>
-                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                    <line x1="10" y1="11" x2="10" y2="17"></line>
-                    <line x1="14" y1="11" x2="14" y2="17"></line>
-                  </svg>
-                  <div className="font-semibold text-sm text-red-700 mt-[3px]">
-                    Delete
-                  </div>
-                </button>
+                    <svg
+                      className="w-6 stroke-red-700"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <polyline points="3 6 5 6 21 6"></polyline>
+                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                      <line x1="10" y1="11" x2="10" y2="17"></line>
+                      <line x1="14" y1="11" x2="14" y2="17"></line>
+                    </svg>
+                    <div className="font-semibold text-sm text-red-700 mt-[3px]">
+                      Delete
+                    </div>
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
